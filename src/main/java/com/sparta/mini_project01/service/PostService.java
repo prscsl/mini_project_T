aa3de754f7358121c03964285910d70307dda467
yeony402 <44489399+yeony402@users.noreply.github.com>
2022-09-05 11:49:57 +0900

    Update PostService.java

--- src/main/java/com/sparta/mini_project01/service/PostService.java
+++ src/main/java/com/sparta/mini_project01/service/PostService.java
@@ -18,83 +18,75 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class PostService {
+    private final S3UploaderService s3Uploader;
+  private final PostRepository postRepository;
+  private final CommentRepository commentRepository;
+  private final TokenProvider tokenProvider;
 
-    private final PostRepository postRepository;
-    private final CommentRepository commentRepository;
-    private final TokenProvider tokenProvider;
+  @Transactional
+  public ResponseDto<?> createPost(PostRequestDto requestDto, HttpServletRequest request, MultipartFile multipartFile) throws IOException {
 
-    @Transactional
-    public ResponseDto<?> createPost(PostRequestDto requestDto, HttpServletRequest request) {
-        if (null == request.getHeader("Refresh-Token")) {
-            return ResponseDto.fail("MEMBER_NOT_FOUND",
-                    "로그인이 필요합니다.");
-        }
-
-        if (null == request.getHeader("Authorization")) {
-            return ResponseDto.fail("MEMBER_NOT_FOUND",
-                    "로그인이 필요합니다.");
-        }
-
-        Member member = validateMember(request);
-        if (null == member) {
-            return ResponseDto.fail("INVALID_TOKEN", "Token이 유효하지 않습니다.");
-        }
-
-        Post post = Post.builder()
-                .title(requestDto.getTitle())
-                .placetitle(requestDto.getPlacetitle())
-                .content(requestDto.getContent())
-                .member(member)
-                .build();
-        postRepository.save(post);
-        return ResponseDto.success(
-                PostResponseDto.builder()
-                        .id(post.getId())
-                        .title(post.getTitle())
-                        .placetitle(post.getPlacetitle())
-                        .content(post.getContent())
-                        .author(post.getMember().getNickname())
-                        .createdAt(post.getCreatedAt())
-                        .modifiedAt(post.getModifiedAt())
-                        .build()
-        );
+    if (null == request.getHeader("Refresh-Token")) {
+      return ResponseDto.fail("MEMBER_NOT_FOUND",
+          "로그인이 필요합니다.");
     }
 
-    @Transactional(readOnly = true)
-    public ResponseDto<?> getPost(Long id) {
-        Post post = isPresentPost(id);
-        if (null == post) {
-            return ResponseDto.fail("NOT_FOUND", "존재하지 않는 게시글 id 입니다.");
-        }
+    if (null == request.getHeader("Authorization")) {
+      return ResponseDto.fail("MEMBER_NOT_FOUND",
+          "로그인이 필요합니다.");
+    }
 
-        List<Comment> commentList = commentRepository.findAllByPost(post);
-        List<CommentResponseDto> commentResponseDtoList = new ArrayList<>();
+    Member member = validateMember(request);
+    if (null == member) {
+      return ResponseDto.fail("INVALID_TOKEN", "Token이 유효하지 않습니다.");
+    }
 
-        for (Comment comment : commentList) {
-            commentResponseDtoList.add(
-                    CommentResponseDto.builder()
-                            .id(comment.getId())
-                            .author(comment.getMember().getNickname())
-                            .content(comment.getContent())
-                            .createdAt(comment.getCreatedAt())
-                            .modifiedAt(comment.getModifiedAt())
-                            .build()
-            );
+    Image image = s3Uploader.uploadFiles(multipartFile,"static/");
 
-        }
+    Post post = Post.builder()
+        .title(requestDto.getTitle())
+        .placetitle(requestDto.getPlacetitle())
+        .content(requestDto.getContent())
+        .imgUrl(image.getPath())
+        .imageKey(image.getKey())
+        .member(member)
+        .build();
+    postRepository.save(post);
+    return ResponseDto.success(
+        PostResponseDto.builder()
+            .id(post.getId())
+            .imageUrl(post.getImgUrl())
+            .placeTitle(post.getPlacetitle())
+            .title(post.getTitle())
+            .content(post.getContent())
+            .author(post.getMember().getNickname())
+            .createdAt(post.getCreatedAt())
+//            .modifiedAt(post.getModifiedAt())
+            .build()
+    );
+  }
 
+  @Transactional(readOnly = true)
+  public ResponseDto<?> getPost(Long id) {
+    Post post = isPresentPost(id);
+    if (null == post) {
+      return ResponseDto.fail("NOT_FOUND", "존재하지 않는 게시글 id 입니다.");
+    }
 
-        return ResponseDto.success(
-                PostResponseDto.builder()
-                        .id(post.getId())
-                        .title(post.getTitle())
-                        .content(post.getContent())
-                        .commentResponseDtoList(commentResponseDtoList)
-                        .author(post.getMember().getNickname())
-                        .createdAt(post.getCreatedAt())
-                        .modifiedAt(post.getModifiedAt())
-                        .build()
-        );
+    List<Comment> commentList = commentRepository.findAllByPost(post);
+    List<CommentResponseDto> commentResponseDtoList = new ArrayList<>();
+
+    for (Comment comment : commentList) {
+
+      commentResponseDtoList.add(
+              CommentResponseDto.builder()
+                      .id(comment.getId())
+                      .author(comment.getMember().getNickname())
+                      .content(comment.getContent())
+                      .createdAt(comment.getCreatedAt())
+                      .modifiedAt(comment.getModifiedAt())
+                      .build()
+      );
     }
 
     @Transactional(readOnly = true)
