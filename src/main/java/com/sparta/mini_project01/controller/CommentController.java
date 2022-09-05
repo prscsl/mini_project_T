62ee91a411e28fc4ed06560a84395950f168c666
yeony402 <44489399+yeony402@users.noreply.github.com>
2022-09-05 18:58:14 +0900

    Update CommentController.java

--- src/main/java/com/sparta/mini_project01/controller/CommentController.java
+++ src/main/java/com/sparta/mini_project01/controller/CommentController.java
@@ -16,10 +16,10 @@ public class CommentController {
 
   private final CommentService commentService;
 
-  @RequestMapping(value = "/api/auth/comment", method = RequestMethod.POST)
-  public ResponseDto<?> createComment(@RequestBody CommentRequestDto requestDto,
+  @RequestMapping(value = "/api/auth/comment/{postId}", method = RequestMethod.POST)
+  public ResponseDto<?> createComment(@PathVariable("postId") Long id, @RequestBody CommentRequestDto requestDto,
       HttpServletRequest request) {
-    return commentService.createComment(requestDto, request);
+    return commentService.createComment(id, requestDto, request);
   }
 
   @RequestMapping(value = "/api/comment/{id}", method = RequestMethod.GET)
@@ -27,16 +27,15 @@ public class CommentController {
     return commentService.getAllCommentsByPost(id);
   }
 
-  @RequestMapping(value = "/api/auth/comment/{id}", method = RequestMethod.PUT)
-  public ResponseDto<?> updateComment(@PathVariable Long id, @RequestBody CommentRequestDto requestDto,
+  @RequestMapping(value = "/api/auth/comment/{commentId}", method = RequestMethod.PUT)
+  public ResponseDto<?> updateComment(@PathVariable("commentId") Long id, @RequestBody CommentRequestDto requestDto,
       HttpServletRequest request) {
     return commentService.updateComment(id, requestDto, request);
   }
 
-  @RequestMapping(value = "/api/auth/comment/{id}", method = RequestMethod.DELETE)
-  public ResponseDto<?> deleteComment(@PathVariable Long id,
+  @RequestMapping(value = "/api/auth/comment/{commentId}", method = RequestMethod.DELETE)
+  public ResponseDto<?> deleteComment(@PathVariable("commentId") Long id,
       HttpServletRequest request) {
     return commentService.deleteComment(id, request);
   }
-
 }
